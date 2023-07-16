be37fee67de2c1b1352dcb4b9ab493a3ceb5a5aa
Tausif Khan <khantausifkhan123@gmail.com>
2023-07-16 11:05:13 +0530

    Hme Page completed

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ import fs from "fs"
 import { fileURLToPath } from "url";
 
 
+
 const app = express();
+
+
 const port = 80;
 const hostname = "127.0.0.1";
 
@@ -16,12 +19,23 @@ const __dirname = path.dirname(__filename);
 //* paths for diffrent foders
 // const pathOf =
 
+// Configure static file serving
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("/", (req, res) => {
-    res.status(200).sendFile(path.join(__dirname +'/src/html/index.html'));
+    res.status(200).sendFile(path.join(__dirname ,'/public/html/index.html'));
 });
 
 app.get("/about",(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname +'/src/html/index.html'));
+    res.status(200).sendFile(path.join(__dirname ,'/public/html/index.html'));
+    
+})
+app.get("/main.css",(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname ,'/public/css/output.css'));
+    
+})
+app.get("/public",(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname +'/public'));
     
 })
 
